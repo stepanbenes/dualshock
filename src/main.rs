c12902c201ee02ab132b17572965f8d7ba7fc50e
Štěpán Beneš <benes.stepan@gmail.com>
2021-02-07 13:51:38 +0100

    Fix main errors

--- src/main.rs
+++ src/main.rs
@@ -1,58 +1,25 @@
-use BlueZ::*;
+//! Example to scan for a short time and then list all known devices.
 
-fn main() {
-    // Create a new session. This establishes the D-Bus connection to talk to BlueZ. In this case we
-    // ignore the join handle, as we don't intend to run indefinitely.
+use bluez_async::BluetoothSession;
+use std::time::Duration;
+use tokio::time;
+
+const SCAN_DURATION: Duration = Duration::from_secs(5);
+
+#[tokio::main]
+async fn main() -> Result<(), eyre::Report> {
+    pretty_env_logger::init();
+
     let (_, session) = BluetoothSession::new().await?;
 
-    // Start scanning for Bluetooth devices, and wait a few seconds for some to be discovered.
+    // Start scanning for Bluetooth devices, and wait a while for some to be discovered.
     session.start_discovery().await?;
-    time::sleep(Duration::from_secs(5)).await;
+    time::sleep(SCAN_DURATION).await;
     session.stop_discovery().await?;
 
-    // Get a list of devices which are currently known.
+    // Get the list of all devices which BlueZ knows about.
     let devices = session.get_devices().await?;
+    println!("Devices: {:#?}", devices);
 
-    // Find the device we care about.
-    let device = devices
-        .into_iter()
-        .find(|device| device.name.as_deref() == Some("My device"))
-        .unwrap();
-
-    // Connect to it.
-    session.connect(&device.id).await?;
-
-    // Look up a GATT service and characteristic by short UUIDs.
-    let service = session
-        .get_service_by_uuid(&device.id, uuid_from_u16(0x1234))
-        .await?;
-    let characteristic = session
-        .get_characteristic_by_uuid(&service.id, uuid_from_u32(0x1235))
-        .await?;
-
-    // Read the value of the characteristic and write a new value.
-    println!(
-        "Value: {:?}",
-        session
-            .read_characteristic_value(&characteristic.id)
-            .await?
-    );
-    session
-        .write_characteristic_value(&characteristic.id, vec![1, 2, 3])
-        .await?;
-
-    // Subscribe to notifications on the characteristic and print them out.
-    let mut events = session
-        .characteristic_event_stream(&characteristic.id)
-        .await?;
-    session.start_notify(&characteristic.id).await?;
-    while let Some(event) = events.next().await {
-        if let BluetoothEvent::Characteristic {
-            id,
-            event: CharacteristicEvent::Value { value },
-        } = event
-        {
-            println!("Update from {}: {:?}", id, value);
-        }
-    }
+    Ok(())
 }
