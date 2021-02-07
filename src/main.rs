d3d8ecb21017c2714da0264e2496878c01cf1203
Štěpán Beneš <benes.stepan@gmail.com>
2021-02-07 14:16:34 +0100

    huhu

--- src/main.rs
+++ src/main.rs
@@ -7,8 +7,8 @@ use tokio::time;
 const SCAN_DURATION: Duration = Duration::from_secs(5);
 
 #[tokio::main]
-async fn main() -> Result<(), eyre::Report> {
-    pretty_env_logger::init();
+async fn main() -> Result<(), std::error:Error> {
+    //pretty_env_logger::init();
 
     let (_, session) = BluetoothSession::new().await?;
 
