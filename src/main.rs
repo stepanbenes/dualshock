efbd87d0934918c6d53e9fb5542101895005b21c
Štěpán Beneš <benes.stepan@gmail.com>
2021-02-08 18:20:13 +0100

    read file

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,20 @@
-extern crate dualshock4;
-extern crate hidapi;
+use std::fs::File;
+use std::io::Read;
 
-use hidapi::HidApi;
+fn get_file_as_byte_vec(filename: &String) -> Vec<u8> {
+    let mut f = File::open(&filename).expect("no file found");
+    let metadata = std::fs::metadata(&filename).expect("unable to read metadata");
+    let mut buffer = vec![0; metadata.len() as usize];
+    f.read(&mut buffer).expect("buffer overflow");
+
+    buffer
+}
 
 fn main() {
-    let api = HidApi::new().expect("Failed to create HID API instance.");
-
-    //println!("{:?}", api.devices().len());
-
-    let controller = dualshock4::get_device(&api).expect("Failed to open device");
-
+    let filename = "C:/temp/test.txt".to_string();
     loop {
-        let data = dualshock4::read(&controller).expect("Failed to read data");
-
-        println!("{:?}", data.buttons.dpad_left);
+        let data = get_file_as_byte_vec(&filename);
+        println!("{:?}", data);
+        std::thread::sleep(std::time::Duration::from_millis(1000));
     }
 }
