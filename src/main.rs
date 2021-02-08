55e4bb77d806df793a713d32ca56d0ae016a620b
Štěpán Beneš <benes.stepan@gmail.com>
2021-02-08 18:38:40 +0100

    joydev

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,12 @@
-use std::fs::File;
-use std::io::Read;
+extern crate joydev;
 
-fn get_file_as_byte_vec(filename: &String) -> Vec<u8> {
-    let mut f = File::open(&filename).expect("no file found");
-    let metadata = std::fs::metadata(&filename).expect("unable to read metadata");
-    let mut buffer = vec![0; metadata.len() as usize];
-    f.read(&mut buffer).expect("buffer overflow");
-
-    buffer
-}
+use joydev::Device;
 
 fn main() {
-    let filename = "C:/temp/test.txt".to_string();
-    loop {
-        let data = get_file_as_byte_vec(&filename);
-        println!("{:?}", data);
-        std::thread::sleep(std::time::Duration::from_millis(1000));
+    // You should probably check what devices are available
+    // by reading /dev/input directory or using udev.
+    if let Ok(device) = Device::open("/dev/input/js0") {
+        // Get an event and print it.
+        println!("{:?}", device.get_event());
     }
 }
